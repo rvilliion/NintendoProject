bdc9962fa3fbc90d0a2a1b2c0641f18d1a2353cc
Emma Faurie <emma.iris.faurie@gmail.com>
2021-10-11 17:50:41 +0200

    creation nouvelle boutique

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -1,7 +1,11 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import metier.Console;
 import metier.Jeu;
+import model.Boutique;
 
 public class Test {
 
@@ -14,6 +18,14 @@ public class Test {
 		Jeu jeuwii3 = new Jeu("Jeu Wii 3", wii);
 		Jeu pokemon = new Jeu("Pokemon", ds);
 		Jeu jeuds2 = new Jeu("jeu DS 2", ds);
+		
+		List<Jeu> listj1 = new ArrayList();
+		listj1.add(pokemon);
+		listj1.add(wiisport);
+		listj1.add(jeuwii2);
+		
+		Boutique b1 = new Boutique("Micromania","Nantes",listj1);
+		
 				
 
 	}
