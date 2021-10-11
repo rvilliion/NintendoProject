4b87afecf3f9df4cfe694c82fdec5e137f2f305e
Emma Faurie <emma.iris.faurie@gmail.com>
2021-10-11 17:18:15 +0200

    add class Client

---
+++ src/main/java/model/Client.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Client {
+	
+	private String nom;
+	private String prenom;
+	private List<Jeu> jeux =new ArrayList();
+	
+
+}
